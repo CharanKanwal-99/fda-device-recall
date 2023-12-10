d0ed3305cf76a0767cf4d94cb7689828db871ace
Charan99 <111955279+CharanKanwal-99@users.noreply.github.com>
2023-12-10 16:27:03 -0700

    Update table_creation.py

--- dags/db/table_creation.py
+++ dags/db/table_creation.py
@@ -7,8 +7,8 @@ def create_table():
     DB_NAME = os.getenv('DB_NAME')
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
+    DB_HOST = 'redshift-cluster-1.c09jeylcxwnb.us-east-2.redshift.amazonaws.com'
+    DB_PORT = 5432
     
     connection = psycopg2.connect(
     dbname=DB_NAME,
