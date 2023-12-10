f53378b341aa7dd80aadf81d85ade10c686eeffc
Charan99 <111955279+CharanKanwal-99@users.noreply.github.com>
2023-12-10 06:10:32 +0530

    Add files via upload

---
+++ dags/db/table_creation.py
@@ -0,0 +1,80 @@
+import psycopg2
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import os
+
+
+def create_table():
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    
+    connection = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+    )
+    
+    db = 'openfda'
+    create_db_query = f'CREATE DATABASE {db};'
+
+    with connection.cursor() as cursor:
+        cursor.execute(create_db_query)
+        connection.close()
+    
+    db_url = 'postgresql+psycopg2://aws_charan:Kewaldotuniv1!@redshift-cluster-1.c09jeylcxwnb.us-east-2.redshift.amazonaws.com:5439/openfda'
+    engine = create_engine(db_url)
+
+# SQLAlchemy metadata object
+    metadata = MetaData()
+
+# Define tables
+    firm_table = Table(
+    'firm',
+    metadata,
+    Column('firm_id', Integer, primary_key=True),
+    Column('firm', String),
+    Column('state', String),
+    Column('city', String),
+    Column('address_1', String),
+    Column('address_2', String),
+    Column('postal_code', String)
+
+)
+
+    status_table = Table(
+    'status',
+    metadata,
+    Column('status_id', Integer, primary_key=True),
+    Column('status', String)
+)
+    status_table = Table(
+    'cause',
+    metadata,
+    Column('cause_id', Integer, primary_key=True),
+    Column('cause', String)
+
+    )
+
+    recall_table = Table(
+    'recall',
+    metadata,
+    Column('recall_id', Integer, primary_key=True),
+    Column('firm_id', Integer),
+    Column('status_id', Integer),
+    Column('cause_id', Integer),
+    Column('product_quantity', String),
+    Column('distribution_pattern', String),
+    Column('product_code', String),
+    Column('device_class', Integer)
+
+
+)
+
+
+
+# Create tables
+    metadata.create_all(engine)
---
+++ dags/etl/data_ingestion.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+from etl.utils import load_object
+
+# Define the API endpoint and parameters
+
+def get_data(**kwargs):
+    api_endpoint = "https://api.fda.gov/device/recall.json"
+    params = {'search': "root_cause_description.exact:'Device Design'", 'limit': 1000}
+
+    # Initialize an empty dataframe to store the data
+    final_df = pd.DataFrame()
+
+    # Make 2 API requests and append data to the dataframe each time
+    for i in range(2):
+        response = requests.get(api_endpoint, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            data = data['results']
+            df = pd.DataFrame(data)
+            df['device_class'] = data[0]['openfda']['device_class']
+            final_df = final_df.append(df, ignore_index=True)
+
+    field_names = ["cfres_id",
+                   "recall_status",
+                   "recalling_firm",
+                   "address_1",
+                   "address_2",
+                   "city",
+                   "state",
+                   "postal_code",
+                   "product_code",
+                   "root_cause_description",
+                   "product_quantity",
+                   "distribution_pattern",
+                   "device_class"]
+    
+    # Export the final dataframe to a CSV file
+    final_df = final_df[field_names]
+    date = kwargs['execution_date'].strftime('%Y-%m-%d')
+    path = f'raw1/{date}_open_fda_raw.csv'
+    load_object(final_df, path)
