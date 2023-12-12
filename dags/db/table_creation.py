28a56dc55bc6945c7b4a15c0d67ec9afe45b389a
Charan99 <111955279+CharanKanwal-99@users.noreply.github.com>
2023-12-12 16:38:28 -0700

    Update table_creation.py

--- dags/db/table_creation.py
+++ dags/db/table_creation.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import os
 
@@ -8,73 +9,57 @@ def create_table():
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = 'redshift-cluster-1.c09jeylcxwnb.us-east-2.redshift.amazonaws.com'
-    DB_PORT = 5432
+    DB_PORT = 5439
     
-    connection = psycopg2.connect(
-    dbname=DB_NAME,
+    conn = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
     user=DB_USER,
     password=DB_PASSWORD,
-    host=DB_HOST,
-    port=DB_PORT
-    )
-    
-    db = 'openfda'
-    create_db_query = f'CREATE DATABASE {db};'
-
-    with connection.cursor() as cursor:
-        cursor.execute(create_db_query)
-        connection.close()
-    
-    db_url = 'postgresql+psycopg2://aws_charan:Kewaldotuniv1!@redshift-cluster-1.c09jeylcxwnb.us-east-2.redshift.amazonaws.com:5439/openfda'
-    engine = create_engine(db_url)
+    database=DB_NAME)
 
 # SQLAlchemy metadata object
-    metadata = MetaData()
-
-# Define tables
-    firm_table = Table(
-    'firm',
-    metadata,
-    Column('firm_id', Integer, primary_key=True),
-    Column('firm', String),
-    Column('state', String),
-    Column('city', String),
-    Column('address_1', String),
-    Column('address_2', String),
-    Column('postal_code', String)
-
-)
-
-    status_table = Table(
-    'status',
-    metadata,
-    Column('status_id', Integer, primary_key=True),
-    Column('status', String)
-)
-    status_table = Table(
-    'cause',
-    metadata,
-    Column('cause_id', Integer, primary_key=True),
-    Column('cause', String)
-
-    )
-
-    recall_table = Table(
-    'recall',
-    metadata,
-    Column('recall_id', Integer, primary_key=True),
-    Column('firm_id', Integer),
-    Column('status_id', Integer),
-    Column('cause_id', Integer),
-    Column('product_quantity', String),
-    Column('distribution_pattern', String),
-    Column('product_code', String),
-    Column('device_class', Integer)
 
 
-)
+    table_queries = ["""CREATE TABLE IF NOT EXISTS firm(
+    firm_id INT PRIMARY KEY,
+    firm VARCHAR,
+    state VARCHAR,
+    city VARCHAR,
+    address_1 VARCHAR,
+    address_2 VARCHAR,
+    postal_code VARCHAR
+    );""",
 
+    """CREATE TABLE IF NOT EXISTS status(
+    status_id INT PRIMARY KEY,
+    status VARCHAR
+    );""",
 
+    """CREATE TABLE IF NOT EXISTS cause (
+    cause_id INT PRIMARY KEY,
+    cause VARCHAR
+    );""",
 
-# Create tables
-    metadata.create_all(engine)
+    """CREATE TABLE IF NOT EXISTS recall(
+    recall_id INT PRIMARY KEY,
+    firm_id INT,
+    status_id INT,
+    cause_id INT,
+    product_quantity VARCHAR,
+    product_code VARCHAR,
+    device_class INT,
+    FOREIGN KEY (firm_id) REFERENCES firm(firm_id),
+    FOREIGN KEY (status_id) REFERENCES status(status_id),
+    FOREIGN KEY (cause_id) REFERENCES cause(cause_id)
+    );"""
+    ]
+
+    cursor = conn.cursor()
+
+    for query in table_queries:
+        cursor.execute(query)
+    print('Tables have been created')
+    conn.commit()
+    cursor.close()
+    conn.close()
